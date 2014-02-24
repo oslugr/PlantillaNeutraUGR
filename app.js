ec20d70e097ebe22da8a39b5304d9be365e3a16d
Jaime <jtbenavente@gmail.com>
2014-02-24 20:30:08 +0100

    añade las plantillas que faltaban

---
+++ app.js
@@ -0,0 +1,59 @@
+/*
+	Plantilla Neutra de la Universidad de Granada
+    Copyright (C) 2014 Jaime Torres Benavente
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/index.html', routes.index);
+app.get('/contacto.html', routes.contacto);
+app.get('/personal.html', routes.personal);
+app.get('/pruebas.html', routes.pruebas);
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+/*
+	Plantilla Neutra de la Universidad de Granada
+    Copyright (C) 2014 Jaime Torres Benavente
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', {});
+};
+
+exports.personal = function(req, res){
+  res.render('personal', {});
+};
+
+exports.contacto = function(req, res){
+  res.render('contacto', {});
+};
+
+exports.pruebas = function(req, res){
+  res.render('pruebas', {});
+};
+
